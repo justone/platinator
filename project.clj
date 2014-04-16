36b2f40a28046763c453871cb781bff207778917
Nate Jones <nate@endot.org>
2014-04-16 12:02:58 -0700

    make lein runnable

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/justone/platinator"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main platinator.core)
--- src/platinator/core.clj
+++ src/platinator/core.clj
@@ -1,6 +1,5 @@
 (ns platinator.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main []
+  (println "I don't do a whole lot.")
+  )
