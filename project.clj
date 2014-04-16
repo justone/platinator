5218a49b8fe8447c0412a47ef65399ba6a00fe4c
Nate Jones <nate@endot.org>
2014-04-16 11:59:35 -0700

    update description and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject platinator "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Platinator: solving the tandem parking problem forever"
+  :url "https://github.com/justone/platinator"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
