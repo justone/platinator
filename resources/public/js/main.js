bc7c3ae7d01bb55d1a002664185c1108993f4346
Nate Jones <nate@endot.org>
2014-05-02 22:36:24 -0700

    rudimentary UI

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -1,1 +1,28 @@
+/** @jsx React.DOM */
+React.renderComponent(
+    <div>
+        <div className="col-md-9">
+            <form className="form-horizontal">
+                <fieldset>
 
+                <div className="control-group">
+                    <div className="input-group">
+                      <span className="input-group-addon">License or Tag</span>
+                      <input type="text" className="form-control" placeholder="1ABC456 or 1234"/>
+                    </div>
+                </div>
+
+                <div className="control-group">
+                  <label className="control-label" htmlFor="singlebutton"></label>
+                  <div className="controls">
+                    <button id="singlebutton" name="singlebutton" className="btn btn-default">Search</button>
+                  </div>
+                </div>
+
+                </fieldset>
+            </form>
+        </div>
+
+    </div>,
+    document.getElementById('main')
+);
